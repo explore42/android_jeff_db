01cd9cee39eb57bd6566466cf87ba0d2ba505c35
explore42 <65012936+explore42@users.noreply.github.com>
2022-04-20 21:34:57 +0800

    0420 night

--- app/src/main/java/com/jeff/android_jeff_db/DeviceAdapter.java
+++ app/src/main/java/com/jeff/android_jeff_db/DeviceAdapter.java
@@ -1,9 +1,14 @@
 package com.jeff.android_jeff_db;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +17,7 @@ import java.util.List;
 
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder> {
     private List<Device> mDeviceList;
+    private Context mContext;//用于传入上下文
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         TextView deviceName;
@@ -19,12 +25,24 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
         TextView deviceLatitude;
         TextView deviceStatus;
 
+        //注册点击事件
+        View deviceView;
+
         public ViewHolder(View itemView) {
             super(itemView);
-            deviceName = (TextView) itemView.findViewById(R.id.device_name);
-            deviceLongitude = (TextView) itemView.findViewById(R.id.device_longitude);
-            deviceLatitude = (TextView) itemView.findViewById(R.id.device_latitude);
-            deviceStatus = (TextView) itemView.findViewById(R.id.device_status);
+            deviceView = itemView;
+
+            //更改之前
+            //deviceName = (TextView) itemView.findViewById(R.id.device_name);
+            //deviceLongitude = (TextView) itemView.findViewById(R.id.device_longitude);
+            //deviceLatitude = (TextView) itemView.findViewById(R.id.device_latitude);
+            //deviceStatus = (TextView) itemView.findViewById(R.id.device_status);
+
+            //更改之后
+            deviceName = (TextView) itemView.findViewById(R.id.device_name_overview);
+            deviceLongitude = (TextView) itemView.findViewById(R.id.device_longitude_overview);
+            deviceLatitude = (TextView) itemView.findViewById(R.id.device_latitude_overview);
+            deviceStatus = (TextView) itemView.findViewById(R.id.device_status_overview);
         }
     }
 
@@ -34,8 +52,38 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
 
     @Override
     public DeviceAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.device_item, parent, false);
+        //View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.device_item, parent, false);
+
+        //更改之后
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.device_first, parent, false);
         ViewHolder holder = new ViewHolder(view);
+
+        mContext = parent.getContext();
+
+        //点击外层布局
+        //跳转到下一层视图
+        holder.deviceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Device device = mDeviceList.get(position);
+                //Toast.makeText(view.getContext(),"you click "+device.getName(),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(mContext, MainActivity2.class);
+                mContext.startActivity(intent);
+
+            }
+        });
+
+        //内部各个子项的点击
+        holder.deviceLongitude.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                Device device = mDeviceList.get(position);
+                Toast.makeText(view.getContext(),"you click longitude "+device.getLongitude(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return holder;
     }
 
@@ -45,7 +93,16 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.ViewHolder
         holder.deviceName.setText(device.getName());
         holder.deviceLongitude.setText(device.getLongitude());
         holder.deviceLatitude.setText(device.getLatitude());
+
         holder.deviceStatus.setText(device.getStatus());
+        //Log.d("color",device.getStatus());
+        if(device.getStatus().equals("1")){
+            //holder.deviceStatus.setBackgroundColor(0x008000);
+            holder.deviceStatus.setBackgroundColor(Color.GREEN);
+        }
+        else {
+            holder.deviceStatus.setBackgroundColor(Color.RED);
+        }
     }
 
     @Override
