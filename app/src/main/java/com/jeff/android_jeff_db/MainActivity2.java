21c25121883a0374334d6a9a35f2713c8b520bb4
explore42 <65012936+explore42@users.noreply.github.com>
2022-04-21 22:56:04 +0800

    0421 xml

--- app/src/main/java/com/jeff/android_jeff_db/MainActivity2.java
+++ app/src/main/java/com/jeff/android_jeff_db/MainActivity2.java
@@ -1,9 +1,5 @@
 package com.jeff.android_jeff_db;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -14,8 +10,20 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity2 extends AppCompatActivity {
     private String triggerName;
@@ -38,8 +46,8 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         triggerName = getIntent().getStringExtra("trigger_name");
-        triggerLo = getIntent().getFloatExtra("trigger_lo",1.000f);
-        triggerLa = getIntent().getFloatExtra("trigger_la",1.000f);
+        triggerLo = getIntent().getFloatExtra("trigger_lo", 103.931f);//后面的是默认值
+        triggerLa = getIntent().getFloatExtra("trigger_la", 30.758f);
         //Log.e("intent",triggerName);
 
         TextView textView = (TextView) findViewById(R.id.textview_detial_cabinet_name);
@@ -50,9 +58,11 @@ public class MainActivity2 extends AppCompatActivity {
         queryDatabaseDevice();
         initDevices();
         initMaps();
+
+        sendRequestWithOKHttp();
     }
 
-    private void queryDatabaseDevice(){
+    private void queryDatabaseDevice() {
         //根据机柜名查询对应的设备，存放在列表中
 
         SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -65,7 +75,7 @@ public class MainActivity2 extends AppCompatActivity {
                 @SuppressLint("Range") String belong = cursor.getString(cursor.getColumnIndex("belong"));
                 @SuppressLint("Range") int status = cursor.getInt(cursor.getColumnIndex("status"));
                 //装入集合之中
-                if(belong.equals(triggerName)){
+                if (belong.equals(triggerName)) {
                     deviceListName.add(name);
                     deviceListBelong.add(belong);
                     deviceListStatus.add(Integer.toString(status));
@@ -79,10 +89,10 @@ public class MainActivity2 extends AppCompatActivity {
         cursor.close();
     }
 
-    private void initDevices(){
+    private void initDevices() {
         //实现对应设备的列表
         for (int i = 0; i < deviceListName.size(); i++) {
-            deviceList.add(new DeviceDetail(deviceListName.get(i),deviceListBelong.get(i),deviceListStatus.get(i)));
+            deviceList.add(new DeviceDetail(deviceListName.get(i), deviceListBelong.get(i), deviceListStatus.get(i)));
         }
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_device_detail);
@@ -93,7 +103,7 @@ public class MainActivity2 extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-    private void initMaps(){
+    private void initMaps() {
         locationMap = (WebView) findViewById(R.id.webview_maps);
         WebSettings webSettings = locationMap.getSettings();
         webSettings.setUseWideViewPort(true);
@@ -102,8 +112,64 @@ public class MainActivity2 extends AppCompatActivity {
 
         //interface
         locationMap.setWebViewClient(new WebViewClient());//用户点击的所有链接都会在您的 WebView 中加载
-        locationMap.addJavascriptInterface(new WebAppInterface(this,triggerLo,triggerLa), "Android");//绑定到Jscript，创建名为 Android 的接口
+        locationMap.addJavascriptInterface(new WebAppInterface(this, triggerLo, triggerLa), "Android");//绑定到Jscript，创建名为 Android 的接口
 
         locationMap.loadUrl("file:///android_asset/web/index.html");
+    }
+
+    //发起网络请求
+    private void sendRequestWithOKHttp() {
+        //先开启了一个子线程
+        // 然后在子线程里使用OkHttp发出一条HTTP请求
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    //Request request = new Request.Builder().url("https://www.baidu.com").build();//最终通过这个build方法展示出这个request，各种办法就包装在request中
+                    Request request = new Request.Builder().url("http://10.0.0.2/get_data.xml").build();//对于模拟器的本地地址
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    //Log.d("https", responseData);
+
+                    parseXMLWithPull(responseData);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+
+            String id = "";
+            String name = "";
+            String version = "";
+
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    //开始解析某个结点
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }
+                        //https://weread.qq.com/web/reader/7c532360718ff6317c5255dk32b321d024832bb90e89958
+                        //明天接着写
+                    }
+
+                    //完成解析某个结点
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
